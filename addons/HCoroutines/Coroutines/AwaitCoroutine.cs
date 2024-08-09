ad15ce98ff017ab4fd7e24957924c5d506633ac3
Binpuki <jpaulvu@gmail.com>
2024-08-08 22:46:26 -0500

    Add HCoroutines

---
+++ addons/HCoroutines/Coroutines/AwaitCoroutine.cs
@@ -0,0 +1,44 @@
+namespace HCoroutines;
+
+using System.Threading.Tasks;
+
+/// <summary>
+/// A coroutine that waits until an asynchronous task has been completed.
+/// If the coroutine is killed before completion, the async task
+/// will currently *not* be canceled.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class AwaitCoroutine<T> : CoroutineBase {
+    public Task<T> Task { get; }
+
+    public AwaitCoroutine(Task<T> task) {
+        Task = task;
+    }
+
+    public override void OnEnter() {
+        // As the CoroutineManager class is not thread safe, ensure that Kill()
+        // is executed on the main Godot thread.
+        TaskScheduler godotTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        Task.ContinueWith(_ => Kill(), godotTaskScheduler);
+    }
+}
+
+/// <summary>
+/// A coroutine that waits until an asynchronous task has been completed.
+/// If the coroutine is killed before completion, the async task
+/// will currently *not* be canceled.
+/// </summary>
+public class AwaitCoroutine : CoroutineBase {
+    public Task Task { get; }
+
+    public AwaitCoroutine(Task task) {
+        Task = task;
+    }
+
+    public override void OnEnter() {
+        // As the CoroutineManager class is not thread safe, ensure that Kill()
+        // is executed on the main Godot thread.
+        TaskScheduler godotTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        Task.ContinueWith(_ => Kill(), godotTaskScheduler);
+    }
+}
---
+++ addons/HCoroutines/HCoroutines.cs
@@ -0,0 +1,16 @@
+#if TOOLS
+namespace HCoroutines;
+
+using Godot;
+
+[Tool]
+public partial class HCoroutines : EditorPlugin {
+    public override void _EnterTree() {
+        // Initialization of the plugin goes here.
+    }
+
+    public override void _ExitTree() {
+        // Clean-up of the plugin goes here.
+    }
+}
+#endif
