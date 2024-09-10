21b6bcc5051a7581f6380812d74efefaf8d781f7
duo <98416575+DuoNineXcore@users.noreply.github.com>
2024-09-10 19:13:25 -0400

    strange subresources behavior

--- source/Rubicon.Autoload/Settings.cs
+++ source/Rubicon.Autoload/Settings.cs
@@ -4,11 +4,8 @@ namespace Rubicon.Autoload;
 public partial class Settings : Node
 {
     private const string SettingsFilePath = "user://settings.tres";
-    private static ClientSettings ClientSettings = new();
-    public static GameplaySettings Gameplay = ClientSettings.GameplaySettings;
-    public static VideoSettings Video = ClientSettings.VideoSettings;
-    public static AudioSettings Audio = ClientSettings.AudioSettings;
-    public static MiscSettings Misc = ClientSettings.MiscSettings;
+    
+    public static ClientSettings ClientSettings { get; private set; } = new();
     
     public override void _Ready()
     {
@@ -20,13 +17,13 @@ public partial class Settings : Node
     {
         if (!ResourceLoader.Exists(SettingsFilePath))
             SaveSettings();
-
+        
         ClientSettings = GD.Load<ClientSettings>(SettingsFilePath);
     }
 
     public static void SaveSettings()
     {
-        ResourceSaver.Save(ClientSettings, SettingsFilePath);
+        ResourceSaver.Save((Resource)ClientSettings, SettingsFilePath);
         GD.Print($"Succesfully saved settings to file: {SettingsFilePath}");
     }
 }
--- source/Rubicon.Data/ClientSettings.cs
+++ source/Rubicon.Data/ClientSettings.cs
@@ -2,28 +2,8 @@ namespace Rubicon.Data;
 
 public partial class ClientSettings : Resource
 {
-    [Export] public GameplaySettings GameplaySettings = new();
-    [Export] public AudioSettings AudioSettings = new();
-    [Export] public VideoSettings VideoSettings = new();
-    [Export] public MiscSettings MiscSettings = new();
-}
-
-public partial class GameplaySettings : Resource
-{
     [Export] public bool Downscroll = false;
-}
-
-public partial class AudioSettings : Resource
-{
     [Export] public int killlll = 0;
-}
-
-public partial class VideoSettings : Resource
-{
     [Export] public bool killpeople = false;
-}
-
-public partial class MiscSettings : Resource
-{
     [Export] public bool arrestifkillpeople = false;
 }
