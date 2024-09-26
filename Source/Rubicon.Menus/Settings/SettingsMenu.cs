c7ab4efdb5630a46a12557c0690933d73e0cddc3
Binpuki <30415792+Binpuki@users.noreply.github.com>
2024-09-25 19:49:30 -0500

    Fixed Settings mouse input and other anchor things

--- Source/Rubicon.Menus/Settings/SettingsMenu.cs
+++ Source/Rubicon.Menus/Settings/SettingsMenu.cs
@@ -2,8 +2,8 @@ using Rubicon.Menus;
 
 public partial class SettingsMenu : BaseMenu
 {
-	[NodePath("AspectRatioContainer/Main/Sidebar/AnimationPlayer")] private AnimationPlayer _sidebarAnimationPlayer;
-	[NodePath("AspectRatioContainer/Main/Sidebar")] private Control _sidebar;
+	[NodePath("Main/Sidebar/AnimationPlayer")] private AnimationPlayer _sidebarAnimationPlayer;
+	[NodePath("Main/Sidebar")] private Control _sidebar;
 
 	public override void _Ready()
 	{
